a217850378ae0d2ae5a1a8d4521a465ce22ea4c0
acslmarc <marc@chezbrown.org>
2023-10-04 18:45:22 -0800

    cleaned up a bit

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+class TaskNameForm(forms.Form):
+    task = forms.CharField(label="Task to delete")
+
+
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task", min_length=5)
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=10, initial=5)
@@ -11,9 +15,9 @@ class NewTaskForm(forms.Form):
         t = self.cleaned_data['task']
         p = self.cleaned_data['priority']
 
-        if p >=4 and len(t) < 10:
+        if p >5 and len(t) < 10:
             self.errors['task'] = self.error_class([
-                'Minimum 10 chars needed when priorty >= 4'
+                'Minimum 10 chars needed when priorty > 5'
             ])
         return self.cleaned_data
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from .forms import NewTaskForm
+from .forms import NewTaskForm, TaskNameForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 
 
-tasks = ['eat', 'sleep', 'pray'
-]
+
 
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
     return render(request, 'tasks/index.html', {
-        'tasks': sorted(tasks),
+        'tasks': sorted(request.session['tasks']),
         'msg': messages
     })
 
@@ -21,34 +22,34 @@ def add(request):
         if f.is_valid():
             task = f.cleaned_data["task"]
             priority = f.cleaned_data["priority"]
-            if task not in tasks:
-                tasks.append(task)
+            if task not in request.session['tasks']:
+                #request.session['tasks'] += [task]
+                request.session['tasks'].append(task)
+                request.session.modified = True
                 messages.success(request, f'Task {task} added.')
                 return redirect('tasks:index')
             else:
                 messages.error(request, f'Duplicate tasks are not allowed')
                 return redirect('tasks:index')
-
         else:
             return render(request, 'tasks/add.html', {"form": f})
     else:
         return render(request, "tasks/add.html", {"form": NewTaskForm()})
 
 def delete(request):
-    if request.method == "DELETE":
-        f = NewTaskForm(request.DELETE)
+    if request.method == "POST":
+        f = TaskNameForm(request.POST)
         if f.is_valid():
             task = f.cleaned_data["task"]
-            priority = f.cleaned_data["priority"]
-            if task in tasks:
-                tasks.remove(task)
+            if task in request.session['tasks']:
+                request.session['tasks'].remove(task)
+                request.session.modified = True
                 messages.success(request, f'Task {task} removed.')
                 return redirect('tasks:index')
             else:
                 messages.error(request, f'This task does not exist')
                 return redirect('tasks:index')
-
         else:
             return render(request, 'tasks/delete.html', {"form": f})
     else:
-        return render(request, "tasks/delete.html", {"form": NewTaskForm()})
+        return render(request, "tasks/delete.html", {"form": TaskNameForm()})
