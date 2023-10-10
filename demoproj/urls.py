d2e7b2260a7e35f11dccbf25732ff1b34f0c0508
Hjriebe <hjriebe@alaska.edu>
2023-10-09 16:25:03 -0800

    ok

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('tasks.urls')),
     path('tasks/', include('tasks.urls')),
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
