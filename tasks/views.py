d1196323efb3afc86cc5458b1b8e54128b4cb841
Hjriebe <hjriebe@alaska.edu>
2023-10-09 17:00:38 -0800

    ok

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import NewTaskForm, TaskNameForm
+from .forms import NewTaskForm, TaskNameForm, TaskForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
@@ -44,3 +44,21 @@ def delete(request):
             return render(request, 'tasks/delete.html', {"form": f})
     else:
         return render(request, "tasks/delete.html", {"form": TaskNameForm()})
+
+
+def add(request):
+    if request.method == "POST":
+        f = TaskForm(request.POST)
+        if f.is_valid():
+            f.save()
+            #task = f.cleaned_data["task"]
+            #priority = f.cleaned_data["priority"]
+            #t = Task(title=task, priority=priority)
+            #t.save()
+            messages.success(request, f'Task {f.cleaned_data("title")} added.')
+            return redirect('tasks:index')
+        else:
+            return render(request, 'tasks/add.html', {"form": f})
+    else:
+        return render(request, "tasks/add.html", {"form": TaskForm()})
+    
